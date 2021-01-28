bc4e29b60d5b572288bf9ba618fb5384838e86d3
Dayana Arrieta Regino <72126244+dparrietar@users.noreply.github.com>
2021-01-27 22:42:49 -0500

    Add files via upload

--- talleres/taller01/Fecha.java
+++ talleres/taller01/Fecha.java
@@ -7,19 +7,19 @@ public class Fecha {
         month = this.month;
         year = this.year;
     }
-    
+
     public int day(){
-       return day;
+        return day;
     }
-    
+
     public int mes(){
         return month;
     }
-    
+
     public int Year(){
         return year;
     }
-    
+
     public int comparar(Fecha otra){
         if(this.year < otra.year){
             return -1;
@@ -34,10 +34,24 @@ public class Fecha {
         }if(this.day > otra.day){
             return 1;
         }return 0;
-        
+
     }
-    
-    public String tostring(){
-        
+
+    public String tostring(int d){
+        String date = "";
+        switch (d){
+            case 1 :
+                date = "this date after other date";
+                break;
+            case 2 :
+                date = "this date before other date";
+                break;
+            case 3 :
+                date = "Equal dates";
+        }
+        return date;
     }
 }
+
+
+
