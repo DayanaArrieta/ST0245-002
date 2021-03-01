14aaccaaf1d5bff6350727fe57e1b9952f096184
Dayana Arrieta Regino <72126244+dparrietar@users.noreply.github.com>
2021-02-28 19:20:01 -0500

    Add files via upload

---
+++ laboratorios/lab01/codigo/ADN.java
@@ -0,0 +1,39 @@
+package Laboratorio1;
+/**
+ * This is an alghorithm that calculates the length of the common subsequence
+ * between two strings chains
+ * @author: Dayana Arrieta & Juan José Quintana.
+ * @version: 26/02/2021
+ */
+public class ADN{
+
+    /**
+     * Metodo que se encarga de calcular la cadena comun más larga entre dos strings.
+     * @param string1 Primer string
+     * @param string2 Segundo string
+     * @return tamaño de la cadena comun más larga.
+     */
+    public static int lcsDNA(String string1, String string2){
+        return lcsDNAAux(string1,string2,string1.length(),string2.length());
+
+    }
+    /**
+     * Metodo auxiliar para lcsDNA
+     * @param string1 Primer string
+     * @param string2 Segundo string
+     * @param m tamaño de string 1
+     * @param n tamaño de string 2
+     * @return tamaño de la cadena comun más larga.
+     */
+    private static int lcsDNAAux(String string1, String string2,int m,int n) {
+        if (m<=0||n<=0)
+        {
+            return 0;
+        }
+
+        else if(string1.charAt(m-1)==string2.charAt(n-1)){
+            return 1 + lcsDNAAux(string1.substring(0,m-1),string2.substring(0,n-1),string1.substring(0,m-1).length(),string2.substring(0,n-1).length());
+        }
+        return Math.max(lcsDNAAux(string1.substring(0,m-1),string2,string1.substring(0,m-1).length(),n), lcsDNAAux(string1,string2.substring(0,n-1),m,string2.substring(0,n-1).length()));
+    }
+}
