d16fa3761e3028dcc3fb70f1127d6938993757ed
Dayana Arrieta Regino <72126244+dparrietar@users.noreply.github.com>
2021-03-10 14:41:08 -0500

    Update MyArrayList.java

--- talleres/taller06/MyArrayList.java
+++ talleres/taller06/MyArrayList.java
@@ -3,10 +3,7 @@ import java.util.Arrays;
 /**
  * La clase MiArrayList tiene la intención de representar un objeto que simule el comportamiento
  * de la clase ya implementada "ArrayList"
- * es claro que no se puede utilizar dicha estructura ya utilizada dentro de este ejercicio.
- * Para más información de la clase ArrayList:
- * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html"> Ver documentacion ArrayList </a>
- * @author Mauricio Toro, Andres Paez
+ * @author Dayana Arrieta, Juan José Quintana
  * @version 1
  */
     public class MyArrayList {
@@ -49,7 +46,7 @@ import java.util.Arrays;
         }
 
         /**
-         * @param i es un íncide donde se encuentra el elemento posicionado
+         * @param i es un indice donde se encuentra el elemento posicionado
          * Retorna el elemento que se encuentra en la posición i de la lista.
          *
          */
@@ -61,7 +58,6 @@ import java.util.Arrays;
          * @param index es la posicion en la cual se va agregar el elemento
          * @param e el elemento a guardar
          * Agrega un elemento e en la posición index de la lista
-         *
          */
         public void add(int index, int e) {
             elements[index] = e;
@@ -69,9 +65,7 @@ import java.util.Arrays;
         }
         /**
          * @param index es la posicion en la cual se va eliminar el elemento
-         *
          * ELimina el elemento  en la posición index de la lista
-         *
          */
         public void del(int index){
             elements.remove(index);
