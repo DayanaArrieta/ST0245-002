d1ce56e9220d11eba370518cb39676545b14cfb4
Dayana Arrieta Regino <72126244+dparrietar@users.noreply.github.com>
2021-03-27 17:51:30 -0500

    Add files via upload

---
+++ laboratorios/lab03/codigo/Main.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+/**
+ * @author Mauricio Toro
+ * @version 1
+ */
+public class Main
+{
+    public static void main() {
+        LectorDatos d = new LectorDatos();
+        HashMap<Long, Node> nodes = d.readNodes();
+        System.out.println(nodes.size());
+        ArrayList<Triplet<Long, Long, Double>> edges = d.readEdges();
+        System.out.println(edges.size());
+        DataStructure e = new DataStructure(nodes, edges);
+        System.out.println(e.adjGraph.size());
+        System.out.println(nodes.get(new Long(287291920)).id);
+
+        Long l = new Long(287291920);
+        Long l2 = new Long(1397149003);
+
+        ArrayList<Long> test1 = e.getSuccessors(l);
+        System.out.println(test1.size());
+
+        for(Long i : test1) {
+            System.out.println(i);
+        }
+        System.out.println("Distance: " + e.getWeight(l, l2));
+    }
+}
