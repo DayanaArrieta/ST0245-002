7aa9a3cd766afdcebf94a5f02d4ca711938ae92f
Dayana Arrieta Regino <72126244+dparrietar@users.noreply.github.com>
2021-04-14 16:52:17 -0500

    Add files via upload
    This code is for using with a single csv file, all you have to do is use the same path for your algorithm and your csv file and it will automaticaly compress your image using the nearest neighbour interpolation method.

---
+++ proyecto/codigo/entrega2/scaling.py
@@ -0,0 +1,24 @@
+import pandas as pd
+df = pd.read_csv('vaca 1.csv')
+#print(df)
+
+import numpy as np
+np_array = df.to_numpy()
+#print(np_array)
+
+import numpy as np
+import cv2
+
+cv2.imwrite("newimg.png", np_array)
+
+#Resize-Scaling
+img = cv2.imread('newimg.png', 1)
+cv2.imshow('Original', img)
+
+img_scale_up = cv2.resize(img, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
+
+cv2.imwrite("vaquita.png", img_scale_up)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
