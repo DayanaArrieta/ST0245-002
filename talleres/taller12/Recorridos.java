fcb3c95f89cc554df5e0a86fa6fe09ecce1ca679
Dayana Arrieta Regino <72126244+dparrietar@users.noreply.github.com>
2021-05-11 23:11:35 -0500

    Add files via upload

---
+++ talleres/taller12/Recorridos.java
@@ -0,0 +1,22 @@
+package Taller12;
+
+class Recorridos{
+    public static boolean hayCaminoDFS(Graph g, int i, int f){
+        return hayCaminoDFS(g, i, f, new boolean[g.size()]);
+    }
+    private static boolean hayCaminoDFS(Graph g, int i, int f, boolean[] v){
+        v[i] = true;
+        if (i == f)
+            return true;
+        //else
+        for (Integer hijito : g.getSuccessors(i))
+            if (v[hijito] == false) // Si estas visitado, no llames recursivamente
+                if (hayCaminoDFS(g,hijito,f,v))
+                    return true;
+        return false; // Si nunca es verdadero o si no tiene hijos, da falso
+    }
+    public static void main(String[] args){
+        Graph g = new DiagraphAM(5);
+        System.out.println(hayCaminoDFS(g, 0, 3));
+    }
+}
